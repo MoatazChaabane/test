e0e2aafab1249ff7e3e97e4554661cac0b9ba4e3
MoatazChaabane <chaabanemoataz@outlook.fr>
2023-08-25 00:04:45 +0100

    MAJ style et fonctionnalité

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,70 @@
 <template>
-  <div id="app">
+  <div id="app" style="height: 100vh; width: 100vw; margin: 0; padding: 0;">
 
-      <!--        date de maintenant-->
+    <!--        date de maintenant-->
 
-      <div
-          style="left: 0px; top: 0px; position: absolute; color: #415C73; font-size: 30px; font-family: Roboto; font-weight: 900; word-wrap: break-word"> Today
-        {{ currentDate }}
-      </div>
+    <div
+        style="left: 0px; top: 0px; position: absolute; color: #415C73; font-size: 30px; font-family: Roboto; font-weight: 900; word-wrap: break-word">
+      Today
+      {{ currentDate }}
+    </div>
     <!--        stop button-->
 
-    <div class="button stop-button" @click="stopTimer" style="background: none; color: #CC5252; text-align: center;">
-      <div style="width: 86px; height: 38px; left: 552px; top: 2px; position: absolute; display: flex; align-items: center; justify-content: center; border-radius: 5px; border: 0.50px #CC5252 solid">
+      <div
+          style="width: 86px; height: 38px; left: 552px; top: 2px; position: absolute; display: flex; align-items: center; justify-content: center; border-radius: 5px; border: 0.50px #CC5252 solid">
         <div class="button stop-button" @click="stopTimer" style="background: none; color: #CC5252;">
           Stop
         </div>
       </div>
     </div>
 
-      <!--        start button-->
-    <div class="button start-button" @click="startTimer" style="background: white;color:#60B669 ">
-      <div style="width: 126px; height: 38px; left: 651px; top: 3px; position: absolute; display: flex; align-items: center; justify-content: center; border-radius: 5px; border: 0.50px #60B669 solid">
-          <div class="button start-button" @click="startTimer" style="background: none;color:#60B669;">
-            Start Now
-          </div>
+    <!--        start button-->
+      <div
+          style="width: 126px; height: 38px; left: 651px; top: 3px; position: absolute; display: flex; align-items: center; justify-content: center; border-radius: 5px; border: 0.50px #60B669 solid">
+        <div class="button start-button" @click="startTimer" style="background: none;color:#60B669;">
+          Start Now
         </div>
       </div>
-    </div>
-<!--      timer-->
-      <div style="left: 809px; top: 8px; position: absolute"><span
-          style="color: #60B669; font-size: 25px; font-family: Roboto; font-weight: 900; word-wrap: break-word">{{
-          formattedPeriod
-        }}</span></div>
-<!--      début du tableau-->
-    <div class="table-container">
-      <table class="table">
-        <tr v-for="(record, index) in sortedRecordedTimes" :key="index">
-          <td class="inner">
-            <div class="period">Period {{ record.id }}</div>
-          </td>
-          <td>
-            <div class="description" style="text-align: right;">{{ record.endTime }}</div>
-          </td>
-          <td class="inner" style="text-align: right;">
-            <div class="time green">{{ formatTime(record.period) }}</div></td>
+  <!--      timer-->
+  <div style="left: 809px; top: 8px; position: absolute"><span
+      style="color: #60B669; font-size: 25px; font-family: Roboto; font-weight: 900; word-wrap: break-word">{{
+      formattedPeriod
+    }}</span></div>
+  <!--      début du tableau-->
+  <div class="table-container">
+    <table class="table">
+      <tr v-for="(record, index) in sortedRecordedTimes" :key="index">
+        <td class="inner">
+          <div class="period">Period {{ record.id }}</div>
+        </td>
+        <td>
+          <div class="description" style="text-align: right;">{{ record.endTime }}</div>
+        </td>
+        <td class="inner" style="text-align: right;">
+          <div class="time green">{{ formatTime(record.period) }}</div>
+        </td>
 
-          <td style="border: none;"></td>
-        </tr>
+        <td style="border: none;"></td>
+      </tr>
 
-        <tr>
-          <td></td>
-          <td style="  color: #6A7480; font-size: 25px; font-family: Roboto; font-weight: 900; word-wrap: break-word; text-align: right">Total</td>
+      <tr>
+        <td></td>
+        <td style="  color: #6A7480; font-size: 25px; font-family: Roboto; font-weight: 900; word-wrap: break-word; text-align: right">
+          Total
+        </td>
 
-          <td style="color: #60B669; font-size: 25px; font-family: Roboto; font-weight: 900; word-wrap: break-word;text-align: right">      {{ formatTime(totalPeriod) }}          </td>
+        <td style="color: #60B669; font-size: 25px; font-family: Roboto; font-weight: 900; word-wrap: break-word;text-align: right">
+          {{ formatTime(totalPeriod) }}
+        </td>
 
-        </tr>
-      </table>
-    </div><!--      TOTAL-->
+      </tr>
+    </table>
+  </div><!--      TOTAL-->
 
 
 </template>
 <script>
-import { ref } from 'vue';
+import {ref} from 'vue';
 
 export default {
   data() {
@@ -91,8 +95,8 @@ export default {
         const endTime = new Date();
         endTime.setSeconds(endTime.getSeconds() - this.currentTime);
 
-        const formattedStartTime = startTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-        const formattedEndTime = endTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+        const formattedStartTime = startTime.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+        const formattedEndTime = endTime.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
 
         this.recordedTimes.push({
           id: this.nextId,
@@ -119,7 +123,7 @@ export default {
     currentDate() {
       const date = new Date();
       const day = date.getDate().toString().padStart(2, '0');
-      const month = date.toLocaleString('default', { month: 'short' });
+      const month = date.toLocaleString('default', {month: 'short'});
       return `${day} ${month}`;
     },
     formattedPeriod() {
